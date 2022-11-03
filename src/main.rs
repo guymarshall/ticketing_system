d3b63e657b1adf2d67390e78f01dfc9adb7e3014
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-03 12:38:43 +0000

    Move ticket struct and impl to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
+use crate::ticket::Ticket;
+
+mod ticket;
+
 fn main() {
-    struct Ticket<'a> {
-        id: i32,
-        title: &'a str,
-        status: &'a str
-    }
-    impl Ticket<'_> {
-        fn get_id(&self) -> i32 {
-            self.id
-        }
-        fn get_title(&self) -> &str {
-            self.title
-        }
-        fn get_status(&self) -> &str {
-            self.status
-        }
-    }
     let new_ticket: Ticket = Ticket { id: 1, title: "A test ticket", status: "Closed" };
 
     println!("ID: {}", new_ticket.get_id());
