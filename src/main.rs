d666b2ff95e1878b3a6a98dac5c48012ff3f59b6
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-03 12:33:14 +0000

    Add basic ticket - more details needed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    struct Ticket<'a> {
+        id: i32,
+        title: &'a str,
+        status: &'a str
+    }
+    impl Ticket<'_> {
+        fn get_id(&self) -> i32 {
+            self.id
+        }
+        fn get_title(&self) -> &str {
+            self.title
+        }
+        fn get_status(&self) -> &str {
+            self.status
+        }
+    }
+    let new_ticket: Ticket = Ticket { id: 1, title: "A test ticket", status: "Closed" };
+
+    println!("ID: {}", new_ticket.get_id());
+    println!("Title: {}", new_ticket.get_title());
+    println!("Status: {}", new_ticket.get_status());
 }
