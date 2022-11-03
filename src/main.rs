b649341a6e4349a4ac6c09b7f3ea60a15b32e170
Guy Marshall <guymarshall9@gmail.com>
2022-11-03 19:07:43 +0000

    Change id to ticket_id

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use crate::ticket::Ticket;
 mod ticket;
 
 fn main() {
-    let new_ticket: Ticket = Ticket { id: 1, title: "A test ticket", status: "Closed" };
+    let new_ticket: Ticket = Ticket { ticket_id: 1, title: "A test ticket", status: "Closed" };
 
-    println!("ID: {}", new_ticket.get_id());
+    println!("ID: {}", new_ticket.get_ticket_id());
     println!("Title: {}", new_ticket.get_title());
     println!("Status: {}", new_ticket.get_status());
 }
--- src/ticket.rs
+++ src/ticket.rs
@@ -1,11 +1,11 @@
 pub struct Ticket<'a> {
-    pub id: i32,
+    pub ticket_id: i32,
     pub title: &'a str,
     pub status: &'a str
 }
 impl Ticket<'_> {
-    pub fn get_id(&self) -> i32 {
-        self.id
+    pub fn get_ticket_id(&self) -> i32 {
+        self.ticket_id
     }
     pub fn get_title(&self) -> &str {
         self.title
