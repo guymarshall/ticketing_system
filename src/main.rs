b351c82a1be47e6f535151eb7c8d2b5654e1fc92
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-07 12:15:10 +0000

    Rename hello to tickets

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ mod page;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-async fn hello() -> impl Responder {
-    let title: &str = "Hello Page";
+async fn tickets() -> impl Responder {
+    let title: &str = "Tickets";
     let html: &str = "
         <p>This is some text! And <b>this</b> word is bold!</p>
         <br>
         <button class=\"btn btn-primary\" onclick=\"alert('This button was clicked!');\">Click me!</button>
     ";
     let js: &str = "";
-    let hello_page: page::Page = page::Page {title: title.to_string(), content: html.to_string(), script_js: js.to_string()};
-    HttpResponse::Ok().body(hello_page.create_page())
+    let tickets_page: page::Page = page::Page {title: title.to_string(), content: html.to_string(), script_js: js.to_string()};
+    HttpResponse::Ok().body(tickets_page.create_page())
 }
 
 pub async fn add_ticket() -> impl Responder {
@@ -40,7 +40,7 @@ pub async fn add_ticket() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/tickets", web::get().to(hello))
+            .route("/tickets", web::get().to(tickets))
             .route("/add_ticket", web::get().to(add_ticket))
     })
     .bind(("127.0.0.1", 8080))?
