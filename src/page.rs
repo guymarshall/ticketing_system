dfffce32243d13b125e301cfff57c62c7cd96123
Guy Marshall <guymarshall9@gmail.com>
2022-11-09 22:19:19 +0000

    Add bootstrap css to page.rs hardcoded

--- src/page.rs
+++ src/page.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 pub struct Page {
     pub title: String,
     pub content: String,
@@ -10,14 +12,16 @@ impl Page {
         let script_js: &String = &self.script_js;
         let app_name: String = "Ticketing System".to_string();
 
-        let head_html: String = String::from("
+        let bootstrap_css: String = fs::read_to_string("bootstrap.css").expect("Unable to read file");
+
+        let head_html: String = format!{"
             <meta charset=\"UTF-8\">
             <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
             <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-            <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">
+            <style>{}</style>
             <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p\" crossorigin=\"anonymous\"></script>
             <script src=\"https://code.jquery.com/jquery-3.6.1.min.js\" integrity=\"sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=\" crossorigin=\"anonymous\"></script>
-        ");
+        ", bootstrap_css};
 
         let body_html: String = format!{"
             <nav class=\"navbar navbar-expand-sm navbar-toggleable-sm navbar-light border-bottom mb-3\">
