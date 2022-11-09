d6a9ceec94f79567459bc569791af16c0861554d
Guy Marshall <guymarshall9@gmail.com>
2022-11-09 22:05:40 +0000

    Add more input fields to new page

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,28 @@ async fn all() -> impl Responder {
 async fn new() -> impl Responder {
     let title: &str = "New";
     let html: &str = "
-        <form method=\"post\">
+        <form method=\"POST\">
             <label for=\"title\">Title: </label>
-            <input type=\"text\" id=\"title\" name=\"title\">
+            <input type=\"text\" name=\"title\" id=\"title\">
             <br>
-            <input type=\"submit\" id=\"save\" name=\"save\">
+            <label for=\"username\">Username: </label>
+            <input type=\"text\" name=\"username\" id=\"username\">
+            <br>
+            <label for=\"description\">Description: </label>
+            <input type=\"text\" name=\"description\" id=\"description\">
+            <br>
+            <label for=\"priority\">Priority: </label>
+            <select name=\"priority\" id=\"priority\">
+                <option value=\"low\">Low</option>
+                <option value=\"medium\">Medium</option>
+                <option value=\"high\">High</option>
+                <option value=\"critical\">Critical</option>
+            </select>
+            <br>
+            <label for=\"comments\">Comments: </label>
+            <input type=\"text\" name=\"comments\" id=\"comments\">
+            <br>
+            <input type=\"submit\" value=\"Submit\">
         </form>
     ";
     let js: &str = "
