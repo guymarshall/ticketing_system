559abe0ea9947fbb7f76f84fef28fc8d1c60cf3b
Guy Marshall <guymarshall9@gmail.com>
2022-12-01 23:29:34 +0000

    Remove fieldset

--- src/main.rs
+++ src/main.rs
@@ -31,42 +31,39 @@ async fn new() -> impl Responder {
     let title: &str = "New";
     let html: &str = "
         <form method=\"POST\">
-            <fieldset>
-                <legend>Add ticket</legend>
-                <div class=\"form-group row\">
-                    <label for=\"title\" class=\"col-sm-2 control-label\">Title: </label>
-                    <div class=\"col-sm-4\">
-                        <input class=\"form-control\" type=\"text\" name=\"title\" id=\"title\">
-                    </div>
-                    <label for=\"name\" class=\"col-sm-2 control-label\">Name: </label>
-                    <div class=\"col-sm-4\">
-                        <input class=\"form-control\" type=\"text\" name=\"name\" id=\"name\">
-                    </div>
+            <div class=\"form-group row\">
+                <label for=\"title\" class=\"col-sm-2 control-label\">Title: </label>
+                <div class=\"col-sm-4\">
+                    <input class=\"form-control\" type=\"text\" name=\"title\" id=\"title\">
                 </div>
-                <div class=\"form-group row\">
-                    <label for=\"priority\" class=\"col-sm-2 control-label\">Priority: </label>
-                    <div class=\"col-sm-4\">
-                        <select class=\"form-control\" name=\"priority\" id=\"priority\">
-                            <option value=\"low\">Low</option>
-                            <option value=\"medium\">Medium</option>
-                            <option value=\"high\">High</option>
-                            <option value=\"critical\">Critical</option>
-                        </select>
-                    </div>
+                <label for=\"name\" class=\"col-sm-2 control-label\">Name: </label>
+                <div class=\"col-sm-4\">
+                    <input class=\"form-control\" type=\"text\" name=\"name\" id=\"name\">
                 </div>
-                <div class=\"form-group row\">
-                    <label for=\"description\" class=\"col-sm-2 control-label\">Description: </label>
-                    <div class=\"col-sm-10\">
-                        <textarea class=\"form-control\" name=\"description\" id=\"description\" rows=\"4\" style=\"overflow: auto; resize: none\"></textarea>
-                    </div>
+            </div>
+            <div class=\"form-group row\">
+                <label for=\"priority\" class=\"col-sm-2 control-label\">Priority: </label>
+                <div class=\"col-sm-4\">
+                    <select class=\"form-control\" name=\"priority\" id=\"priority\">
+                        <option value=\"low\">Low</option>
+                        <option value=\"medium\">Medium</option>
+                        <option value=\"high\">High</option>
+                        <option value=\"critical\">Critical</option>
+                    </select>
                 </div>
-                <div class=\"form-group row\">
-                    <label for=\"comments\" class=\"col-sm-2 control-label\">Comments: </label>
-                    <div class=\"col-sm-10\">
-                        <textarea class=\"form-control\" name=\"comments\" id=\"comments\" rows=\"4\" style=\"overflow: auto; resize: none\"></textarea>
-                    </div>
+            </div>
+            <div class=\"form-group row\">
+                <label for=\"description\" class=\"col-sm-2 control-label\">Description: </label>
+                <div class=\"col-sm-10\">
+                    <textarea class=\"form-control\" name=\"description\" id=\"description\" rows=\"4\" style=\"overflow: auto; resize: none\"></textarea>
                 </div>
-            </fieldset>
+            </div>
+            <div class=\"form-group row\">
+                <label for=\"comments\" class=\"col-sm-2 control-label\">Comments: </label>
+                <div class=\"col-sm-10\">
+                    <textarea class=\"form-control\" name=\"comments\" id=\"comments\" rows=\"4\" style=\"overflow: auto; resize: none\"></textarea>
+                </div>
+            </div>
             <button type=\"button\" class=\"btn btn-outline-primary\">Submit</button>
         </form>
     ";
