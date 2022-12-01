d78f03569ea1649f3c1b32697dfacbf3cbbb245e
Guy Marshall <guymarshall9@gmail.com>
2022-12-01 23:24:33 +0000

    Remove selectfeedback

--- src/main.rs
+++ src/main.rs
@@ -35,17 +35,17 @@ async fn new() -> impl Responder {
                 <legend>Add ticket</legend>
                 <div class=\"form-group row\">
                     <label for=\"title\" class=\"col-sm-2 control-label\">Title: </label>
-                    <div class=\"col-sm-4 selectFeedback\">
+                    <div class=\"col-sm-4\">
                         <input class=\"form-control\" type=\"text\" name=\"title\" id=\"title\">
                     </div>
                     <label for=\"name\" class=\"col-sm-2 control-label\">Name: </label>
-                    <div class=\"col-sm-4 selectFeedback\">
+                    <div class=\"col-sm-4\">
                         <input class=\"form-control\" type=\"text\" name=\"name\" id=\"name\">
                     </div>
                 </div>
                 <div class=\"form-group row\">
                     <label for=\"priority\" class=\"col-sm-2 control-label\">Priority: </label>
-                    <div class=\"col-sm-4 selectFeedback\">
+                    <div class=\"col-sm-4\">
                         <select class=\"form-control\" name=\"priority\" id=\"priority\">
                             <option value=\"low\">Low</option>
                             <option value=\"medium\">Medium</option>
@@ -56,13 +56,13 @@ async fn new() -> impl Responder {
                 </div>
                 <div class=\"form-group row\">
                     <label for=\"description\" class=\"col-sm-2 control-label\">Description: </label>
-                    <div class=\"col-sm-10 selectFeedback\">
+                    <div class=\"col-sm-10\">
                         <textarea class=\"form-control\" name=\"description\" id=\"description\" rows=\"4\" style=\"overflow: auto; resize: none\"></textarea>
                     </div>
                 </div>
                 <div class=\"form-group row\">
                     <label for=\"comments\" class=\"col-sm-2 control-label\">Comments: </label>
-                    <div class=\"col-sm-10 selectFeedback\">
+                    <div class=\"col-sm-10\">
                         <textarea class=\"form-control\" name=\"comments\" id=\"comments\" rows=\"4\" style=\"overflow: auto; resize: none\"></textarea>
                     </div>
                 </div>
